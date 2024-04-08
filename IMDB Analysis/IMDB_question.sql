8b8cf57b7ff88664ed2b74af12ce4f4979b958c0
Komal Gupta <95182287+KG-GitHubRepo@users.noreply.github.com>
2024-04-08 18:13:55 +0530

    Update Questions.sql

--- IMDB Analysis/IMDB_question.sql
+++ IMDB Analysis/IMDB_question.sql
@@ -1,39 +1,11 @@
-USE imdb;
-
-/* Now that you have imported the data sets, let’s explore some of the tables. 
- To begin with, it is beneficial to know the shape of the tables and whether any column has null values.
- Further in this segment, you will take a look at 'movies' and 'genre' tables.*/
-
-
-
 -- Segment 1:
 
-
-
-
 -- Q1. Find the total number of rows in each table of the schema?
--- Type your code below:
-
-
-
-
-
-
-
 
 
 -- Q2. Which columns in the movie table have null values?
--- Type your code below:
 
 
-
-
-
-
-
-
-
--- Now as you can see four columns of the movie table has null values. Let's look at the at the movies released each year. 
 -- Q3. Find the total number of movies released each year? How does the trend look month wise? (Output expected)
 
 /* Output format for the first part:
@@ -55,88 +27,25 @@ Output format for the second part of the question:
 |	2			|	 231			|
 |	.			|		.			|
 +---------------+-------------------+ */
--- Type your code below:
 
 
-
-
-
-
-
-
-
-
-/*The highest number of movies is produced in the month of March.
-So, now that you have understood the month-wise trend of movies, let’s take a look at the other details in the movies table. 
-We know USA and India produces huge number of movies each year. Lets find the number of movies produced by USA or India for the last year.*/
   
 -- Q4. How many movies were produced in the USA or India in the year 2019??
--- Type your code below:
 
 
-
-
-
-
-
-
-
-
-/* USA and India produced more than a thousand movies(you know the exact number!) in the year 2019.
-Exploring table Genre would be fun!! 
-Let’s find out the different genres in the dataset.*/
 
 -- Q5. Find the unique list of the genres present in the data set?
--- Type your code below:
 
 
-
-
-
-
-
-
-
-
-/* So, RSVP Movies plans to make a movie of one of these genres.
-Now, wouldn’t you want to know which genre had the highest number of movies produced in the last year?
-Combining both the movie and genres table can give more interesting insights. */
 
 -- Q6.Which genre had the highest number of movies produced overall?
--- Type your code below:
 
-
-
-
-
-
-
-
-
-
-/* So, based on the insight that you just drew, RSVP Movies should focus on the ‘Drama’ genre. 
-But wait, it is too early to decide. A movie can belong to two or more genres. 
-So, let’s find out the count of movies that belong to only one genre.*/
 
 -- Q7. How many movies belong to only one genre?
--- Type your code below:
 
 
-
-
-
-
-
-
-
-
-/* There are more than three thousand movies which has only one genre associated with them.
-So, this figure appears significant. 
-Now, let's find out the possible duration of RSVP Movies’ next project.*/
 
 -- Q8.What is the average duration of movies in each genre? 
--- (Note: The same movie can belong to multiple genres.)
-
 
 /* Output format:
 
@@ -147,21 +56,10 @@ Now, let's find out the possible duration of RSVP Movies’ next project.*/
 |	.			|		.			|
 |	.			|		.			|
 +---------------+-------------------+ */
--- Type your code below:
 
 
-
-
-
-
-
-
-
-/* Now you know, movies of genre 'Drama' (produced highest in number in 2019) has the average duration of 106.77 mins.
-Lets find where the movies of genre 'thriller' on the basis of number of movies.*/
 
 -- Q9.What is the rank of the ‘thriller’ genre of movies among all the genres in terms of number of movies produced? 
--- (Hint: Use the Rank function)
 
 
 /* Output format:
@@ -170,28 +68,9 @@ Lets find where the movies of genre 'thriller' on the basis of number of movies.
 +---------------+-------------------+---------------------+
 |drama			|	2312			|			2		  |
 +---------------+-------------------+---------------------+*/
--- Type your code below:
-
-
-
-
-
-
-
-
-
-
-/*Thriller movies is in top 3 among all genres in terms of number of movies
- In the previous segment, you analysed the movies and genres tables. 
- In this segment, you will analyse the ratings table as well.
-To start with lets get the min and max values of different columns in the table*/
-
-
 
 
 -- Segment 2:
-
-
 
 
 -- Q10.  Find the minimum and maximum values in  each column of the ratings table except the movie_id column?
@@ -201,18 +80,7 @@ To start with lets get the min and max values of different columns in the table*
 +---------------+-------------------+---------------------+----------------------+-----------------+-----------------+
 |		0		|			5		|	       177		  |	   2000	    		 |		0	       |	8			 |
 +---------------+-------------------+---------------------+----------------------+-----------------+-----------------+*/
--- Type your code below:
 
-
-
-
-
-
-    
-
-/* So, the minimum and maximum values in each column of the ratings table are in the expected range. 
-This implies there are no outliers in the table. 
-Now, let’s find out the top 10 movies based on average rating.*/
 
 -- Q11. Which are the top 10 movies based on average rating?
 /* Output format:
@@ -224,19 +92,8 @@ Now, let’s find out the top 10 movies based on average rating.*/
 |	.			|		.			|			.		  |
 |	.			|		.			|			.		  |
 +---------------+-------------------+---------------------+*/
--- Type your code below:
--- It's ok if RANK() or DENSE_RANK() is used too
 
 
-
-
-
-
-
-
-/* Do you find you favourite movie FAN in the top 10 movies with an average rating of 9.6? If not, please check your code again!!
-So, now that you know the top 10 movies, do you think character actors and filler actors can be from these movies?
-Summarising the ratings table based on the movie counts by median rating can give an excellent insight.*/
 
 -- Q12. Summarise the ratings table based on the movie counts by median ratings.
 /* Output format:
@@ -248,20 +105,8 @@ Summarising the ratings table based on the movie counts by median rating can giv
 |	.			|		.			|
 |	.			|		.			|
 +---------------+-------------------+ */
--- Type your code below:
--- Order by is good to have
 
 
-
-
-
-
-
-
-
-
-/* Movies with a median rating of 7 is highest in number. 
-Now, let's find out the production house with which RSVP Movies can partner for its next project.*/
 
 -- Q13. Which production house has produced the most number of hit movies (average rating > 8)??
 /* Output format:
@@ -270,18 +115,7 @@ Now, let's find out the production house with which RSVP Movies can partner for 
 +------------------+-------------------+---------------------+
 | The Archers	   |		1		   |			1	  	 |
 +------------------+-------------------+---------------------+*/
--- Type your code below:
 
-
-
-
-
-
-
-
-
--- It's ok if RANK() or DENSE_RANK() is used too
--- Answer can be Dream Warrior Pictures or National Theatre Live or both
 
 -- Q14. How many movies released in each genre during March 2017 in the USA had more than 1,000 votes?
 /* Output format:
@@ -293,17 +127,8 @@ Now, let's find out the production house with which RSVP Movies can partner for 
 |	.			|		.			|
 |	.			|		.			|
 +---------------+-------------------+ */
--- Type your code below:
 
 
-
-
-
-
-
-
-
--- Lets try to analyse with a unique problem statement.
 -- Q15. Find movies of each genre that start with the word ‘The’ and which have an average rating > 8?
 /* Output format:
 +---------------+-------------------+---------------------+
@@ -314,50 +139,18 @@ Now, let's find out the production house with which RSVP Movies can partner for 
 |	.			|		.			|			.		  |
 |	.			|		.			|			.		  |
 +---------------+-------------------+---------------------+*/
--- Type your code below:
 
 
 
-
-
-
-
-
-
--- You should also try your hand at median rating and check whether the ‘median rating’ column gives any significant insights.
 -- Q16. Of the movies released between 1 April 2018 and 1 April 2019, how many were given a median rating of 8?
--- Type your code below:
 
 
 
-
-
-
-
-
-
--- Once again, try to solve the problem given below.
 -- Q17. Do German movies get more votes than Italian movies? 
 -- Hint: Here you have to find the total number of votes for both German and Italian movies.
--- Type your code below:
-
-
-
-
-
-
-
-
--- Answer is Yes
-
-/* Now that you have analysed the movies, genres and ratings tables, let us now analyse another table, the names table. 
-Let’s begin by searching for null values in the tables.*/
-
-
 
 
 -- Segment 3:
-
 
 
 -- Q18. Which columns in the names table have null values??
@@ -367,18 +160,8 @@ Let’s begin by searching for null values in the tables.*/
 +---------------+-------------------+---------------------+----------------------+
 |		0		|			123		|	       1234		  |	   12345	    	 |
 +---------------+-------------------+---------------------+----------------------+*/
--- Type your code below:
 
 
-
-
-
-
-
-
-/* There are no Null value in the column 'name'.
-The director is the most important person in a movie crew. 
-Let’s find out the top three directors in the top three genres who can be hired by RSVP Movies.*/
 
 -- Q19. Who are the top three directors in the top three genres whose movies have an average rating > 8?
 -- (Hint: The top three genres would have the most number of movies with an average rating > 8.)
@@ -391,18 +174,7 @@ Let’s find out the top three directors in the top three genres who can be hire
 |	.			|		.			|
 |	.			|		.			|
 +---------------+-------------------+ */
--- Type your code below:
 
-
-
-
-
-
-
-
-
-/* James Mangold can be hired as the director for RSVP's next project. Do you remeber his movies, 'Logan' and 'The Wolverine'. 
-Now, let’s find out the top two actors.*/
 
 -- Q20. Who are the top two actors whose movies have a median rating >= 8?
 /* Output format:
@@ -413,18 +185,8 @@ Now, let’s find out the top two actors.*/
 |Christain Bale	|		10			|
 |	.			|		.			|
 +---------------+-------------------+ */
--- Type your code below:
 
 
-
-
-
-
-
-
-/* Have you find your favourite actor 'Mohanlal' in the list. If no, please check your code again. 
-RSVP Movies plans to partner with other global production houses. 
-Let’s find out the top three production houses in the world.*/
 
 -- Q21. Which are the top three production houses based on the number of votes received by their movies?
 /* Output format:
@@ -435,23 +197,8 @@ Let’s find out the top three production houses in the world.*/
 |	.				|		.			|			.		  |
 |	.				|		.			|			.		  |
 +-------------------+-------------------+---------------------+*/
--- Type your code below:
 
 
-
-
-
-
-
-
-
-
-/*Yes Marvel Studios rules the movie world.
-So, these are the top three production houses based on the number of votes received by the movies they have produced.
-
-Since RSVP Movies is based out of Mumbai, India also wants to woo its local audience. 
-RSVP Movies also wants to hire a few Indian actors for its upcoming project to give a regional feel. 
-Let’s find who these actors could be.*/
 
 -- Q22. Rank actors with movies released in India based on their average ratings. Which actor is at the top of the list?
 -- Note: The actor should have acted in at least five Indian movies. 
@@ -466,17 +213,8 @@ Let’s find who these actors could be.*/
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
 +---------------+-------------------+---------------------+----------------------+-----------------+*/
--- Type your code below:
 
 
-
-
-
-
-
-
-
--- Top actor is Vijay Sethupathi
 
 -- Q23.Find out the top five actresses in Hindi movies released in India based on their average ratings? 
 -- Note: The actresses should have acted in at least three Indian movies. 
@@ -490,18 +228,7 @@ Let’s find who these actors could be.*/
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
 +---------------+-------------------+---------------------+----------------------+-----------------+*/
--- Type your code below:
 
-
-
-
-
-
-
-
-
-/* Taapsee Pannu tops with average rating 7.74. 
-Now let us divide all the thriller movies in the following categories and find out their numbers.*/
 
 
 /* Q24. Select thriller movies as per avg rating and classify them in the following category: 
@@ -511,18 +238,7 @@ Now let us divide all the thriller movies in the following categories and find o
 			Rating between 5 and 7: One-time-watch movies
 			Rating < 5: Flop movies
 --------------------------------------------------------------------------------------------*/
--- Type your code below:
 
-
-
-
-
-
-
-
-
-/* Until now, you have analysed various tables of the data set. 
-Now, you will perform some tasks that will give you a broader understanding of the data in this segment.*/
 
 -- Segment 4:
 
@@ -537,20 +253,7 @@ Now, you will perform some tasks that will give you a broader understanding of t
 |		.		|			.		|	       .		  |	   .	    		 |
 |		.		|			.		|	       .		  |	   .	    		 |
 +---------------+-------------------+---------------------+----------------------+*/
--- Type your code below:
 
-
-
-
-
-
-
-
-
--- Round is good to have and not a must have; Same thing applies to sorting
-
-
--- Let us find top 5 movies of each year with top 3 genres.
 
 -- Q26. Which are the five highest-grossing movies of each year that belong to the top three genres? 
 -- (Note: The top 3 genres would have the most number of movies.)
@@ -564,20 +267,9 @@ Now, you will perform some tasks that will give you a broader understanding of t
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
 +---------------+-------------------+---------------------+----------------------+-----------------+*/
--- Type your code below:
-
--- Top 3 Genres based on most number of movies
 
 
 
-
-
-
-
-
-
-
--- Finally, let’s find out the names of the top two production houses that have produced the highest number of hits among multilingual movies.
 -- Q27.  Which are the top two production houses that have produced the highest number of hits (median rating >= 8) among multilingual movies?
 /* Output format:
 +-------------------+-------------------+---------------------+
@@ -587,17 +279,7 @@ Now, you will perform some tasks that will give you a broader understanding of t
 |	.				|		.			|			.		  |
 |	.				|		.			|			.		  |
 +-------------------+-------------------+---------------------+*/
--- Type your code below:
 
-
-
-
-
-
-
-
--- Multilingual is the important piece in the above question. It was created using POSITION(',' IN languages)>0 logic
--- If there is a comma, that means the movie is of more than one language
 
 
 -- Q28. Who are the top 3 actresses based on number of Super Hit movies (average rating >8) in drama genre?
@@ -608,13 +290,7 @@ Now, you will perform some tasks that will give you a broader understanding of t
 |	Laura Dern	|			1016	|	       1		  |	   9.60			     |		1	       |
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
 |		.		|			.		|	       .		  |	   .	    		 |		.	       |
-+---------------+-------------------+---------------------+----------------------+-----------------+*/
--- Type your code below:
-
-
-
-
-
++---------------+-------------------+---------------------+----------------------+-----------------+*
 
 
 
@@ -645,11 +321,4 @@ Format:
 +---------------+-------------------+---------------------+----------------------+--------------+--------------+------------+------------+----------------+
 
 --------------------------------------------------------------------------------------------*/
--- Type you code below:
-
-
-
-
-
-
 
